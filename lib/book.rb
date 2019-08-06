566aefce8da186b4c0ac30e0e953db04150eed73
daniel Woodford <daniel.woodford.g@gmail.com>
2019-08-06 18:01:37 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,19 @@
 class Book
- def initialize(title)
-    @title = title
- end 
-  
-    def title 
-      @title
-    end
-end 
+    def initialize(title)
+     @title = title
+     end 
+     
+       def title 
+         @title
+       end
 
+       def author
+        @author
+       end
+
+       def author=(author)
+        @author = author
+       end
+   end 
+   
+   
